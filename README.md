254c41cd455f5aef83ed55138087d2d439850cd6
Nirant <nirant.bits@gmail.com>
2018-10-24 11:18:53 +0530

    Finish version1 of Wrapper

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # fitz-wrapper
 CLI Utilities for PDF to Image Conversion, built with Py3
+
+## Installation
+
+### 1. Download
+Git clone this repo:
+
+`git clone https://github.com/NirantK/fitz-wrapper.git`
+
+OR
+
+[Download as zip](https://github.com/NirantK/fitz-wrapper/archive/master.zip) and then unzip
+
+### 2. Setup
+In your terminal/shell, `cd` to the `fitz-wrapper` directory and install all dependencies with:
+
+`pip install -r requirements.txt`
+
+## Usage
+
+Extract images from a sample pdf:
+```
+Usage:       extract_images.py PDF OUT
+             extract_images.py --pdf PDF --out OUT
+```
+
+Example Usage:
+
+```bash
+python extract_images.py --pdf sample.pdf --out sample_out extract
+```
+
+You can see more help with:
+`python extract_images.py -- --help`
---
+++ extract_images.py
@@ -0,0 +1,51 @@
+import fire
+import fitz
+from pathlib import Path
+import json
+from typing import List
+
+class Convertor(object):
+    """
+
+    """
+    def __init__(self, pdf: str, out: str):
+        self.document_path = pdf
+        assert Path(self.document_path).exists()
+        self.output_dir = Path(out)
+        if not self.output_dir.exists():
+            self.output_dir.mkdir(exist_ok=True)
+
+    def extract(self)->List[str]:
+        """
+        This function convert pdf pages into PNG images. To convert pdf pages to png images, fitz package is used.
+
+        Args:
+            document_path (str): Path of the pdf document.
+
+        Returns:
+            List of images, path to temporary file and path to temporary directory.
+
+        Raises:
+            If any error occurs or fitz package call fails.
+        """
+        doc = fitz.open(self.document_path)
+        image_filenames = []
+        for i in range(len(doc)):
+            print(f"Page {i+1} | Number of Images: {len(doc.getPageImageList(i))}")
+            for img in doc.getPageImageList(i):
+                xref = img[0]
+                pix = fitz.Pixmap(doc, xref)
+                filename = str(self.output_dir / f"page{i}-image{xref}.png")
+                if pix.n < 5:  # this is GRAY or RGB
+                    pix.writePNG(filename)
+                    image_filenames.append(filename)
+                else:  # CMYK: convert to RGB first
+                    pix1 = fitz.Pixmap(fitz.csRGB, pix)
+                    pix1.writePNG(filename)
+                    image_filenames.append(filename)
+                    pix1 = None
+                pix = None
+        print(json.dumps(image_filenames, indent=2))
+
+if __name__ == "__main__":
+    fire.Fire(Convertor)
