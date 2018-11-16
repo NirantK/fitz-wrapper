7ca99bbdb7da7e9e48768950061cff68f718de2d
Nirant K <nirant@soroco.com>
2018-11-16 11:44:25 +0000

    Add image conversion

--- README.md
+++ README.md
@@ -21,15 +21,25 @@ In your terminal/shell, `cd` to the `fitz-wrapper` directory and install all dep
 
 Extract images from a sample pdf:
 ```
-Usage:       extract_images.py PDF OUT
-             extract_images.py --pdf PDF --out OUT
+Usage:    python pdf_image_handling.py --pdf PDF --out OUT {convert, extract}
 ```
 
-Example Usage:
+Example Usage for Extracting Images:
 
 ```bash
-python extract_images.py --pdf sample.pdf --out sample_out extract
+python pdf_image_handling.py --pdf sample.pdf --out sample_out extract
 ```
 
+This will extract all images in your pdf to a directory called `sample_out` created relative to present directory
+
+
+Example Usage for Converting to Images:
+
+```bash
+python -m pdf_image_handling --pdf sample.pdf --out output convert
+```
+
+This will convert all the pdf pages to images (1 image per page) in your pdf to a directory called `output`. This directory is created relative to present directory
+
 You can see more help with:
-`python extract_images.py -- --help`
+`python pdf_image_handling.py -- --help`
---
+++ pdf_image_handling.py
@@ -0,0 +1,78 @@
+import json
+from pathlib import Path
+from typing import List
+import fire
+import fitz
+
+
+class Converter(object):
+    """
+    """
+
+    def __init__(self, pdf: str, out: str):
+        self.document_path = pdf
+        assert Path(self.document_path).exists()
+        self.output_dir = Path(out)
+        if not self.output_dir.exists():
+            self.output_dir.mkdir(exist_ok=True)
+
+    def extract(self) -> List[str]:
+        """
+        This function convert pdf pages into PNG images. To convert pdf pages to png images, fitz package is used.
+
+        Args:
+            document_path (str): Path of the pdf document.
+
+        Returns:
+            List of images, path to temporary file and path to temporary directory.
+
+        Raises:
+            If any error occurs or fitz package call fails.
+        """
+        doc = fitz.open(self.document_path)
+        image_filenames = []
+        for i in range(len(doc)):
+            print(f"Page {i+1} | Number of Images: {len(doc.getPageImageList(i))}")
+            for img in doc.getPageImageList(i):
+                xref = img[0]
+                pix = fitz.Pixmap(doc, xref)
+                filename = str(self.output_dir / f"page{i}-image{xref}.png")
+                if pix.n < 5:  # this is GRAY or RGB
+                    pix.writePNG(filename)
+                    image_filenames.append(filename)
+                else:  # CMYK: convert to RGB first
+                    pix1 = fitz.Pixmap(fitz.csRGB, pix)
+                    pix1.writePNG(filename)
+                    image_filenames.append(filename)
+                    pix1 = None
+                pix = None
+        print(json.dumps(image_filenames, indent=2))
+
+    def convert(self) -> List[str]:
+        """
+        This function convert pdf pages into PNG images. To convert pdf pages to png images, fitz package is used.
+
+        Args:
+            document_path (str): Path of the pdf document.
+
+        Returns:
+            List of images, path to temporary file and path to temporary directory.
+
+        Raises:
+            If any error occurs or fitz package call fails.
+
+        """
+        doc = fitz.open(self.document_path)
+        image_filenames = []
+
+        for i, page in enumerate(doc):  # iterate through the pages
+            pix = page.getPixmap(alpha=False)  # render page to an image
+            filename = str(self.output_dir / f"page{i}-image.png")
+            pix.writePNG(filename)  # store image as a PNG
+            image_filenames.append(filename)
+
+        print(json.dumps(image_filenames, indent=2))
+
+
+if __name__ == "__main__":
+    fire.Fire(Converter)
